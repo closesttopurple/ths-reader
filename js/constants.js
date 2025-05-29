1ac75fdfd4baad4351049401accb370a594a70ef
closesttopurple <appleapples3500@gmail.com>
2025-05-29 04:45:19 -0400

    modified:   LICENSE 	deleted:    README.md 	modified:   js/constants.js 	new file:   library/config.json 	new file:   library/drakengard-judgement/1/1.jpg 	new file:   library/drakengard-judgement/1/10.jpg 	new file:   library/drakengard-judgement/1/11.jpg 	new file:   library/drakengard-judgement/1/12.jpg 	new file:   library/drakengard-judgement/1/13.jpg 	new file:   library/drakengard-judgement/1/14.jpg 	new file:   library/drakengard-judgement/1/15.jpg 	new file:   library/drakengard-judgement/1/16.jpg 	new file:   library/drakengard-judgement/1/17.jpg 	new file:   library/drakengard-judgement/1/18.jpg 	new file:   library/drakengard-judgement/1/19.jpg 	new file:   library/drakengard-judgement/1/2.jpg 	new file:   library/drakengard-judgement/1/20.jpg 	new file:   library/drakengard-judgement/1/21.jpg 	new file:   library/drakengard-judgement/1/22.jpg 	new file:   library/drakengard-judgement/1/23.jpg 	new file:   library/drakengard-judgement/1/24.jpg 	new file:   library/drakengard-judgement/1/25.jpg 	new file:   library/drakengard-judgement/1/26.jpg 	new file:   library/drakengard-judgement/1/27.jpg 	new file:   library/drakengard-judgement/1/28.jpg 	new file:   library/drakengard-judgement/1/29.jpg 	new file:   library/drakengard-judgement/1/3.jpg 	new file:   library/drakengard-judgement/1/30.jpg 	new file:   library/drakengard-judgement/1/31.jpg 	new file:   library/drakengard-judgement/1/32.jpg 	new file:   library/drakengard-judgement/1/33.jpg 	new file:   library/drakengard-judgement/1/34.jpg 	new file:   library/drakengard-judgement/1/35.jpg 	new file:   library/drakengard-judgement/1/36.jpg 	new file:   library/drakengard-judgement/1/37.jpg 	new file:   library/drakengard-judgement/1/38.jpg 	new file:   library/drakengard-judgement/1/39.jpg 	new file:   library/drakengard-judgement/1/4.jpg 	new file:   library/drakengard-judgement/1/40.jpg 	new file:   library/drakengard-judgement/1/41.jpg 	new file:   library/drakengard-judgement/1/42.jpg 	new file:   library/drakengard-judgement/1/43.jpg 	new file:   library/drakengard-judgement/1/44.jpg 	new file:   library/drakengard-judgement/1/45.jpg 	new file:   library/drakengard-judgement/1/46.jpg 	new file:   library/drakengard-judgement/1/47.jpg 	new file:   library/drakengard-judgement/1/48.jpg 	new file:   library/drakengard-judgement/1/49.jpg 	new file:   library/drakengard-judgement/1/5.jpg 	new file:   library/drakengard-judgement/1/50.jpg 	new file:   library/drakengard-judgement/1/51.jpg 	new file:   library/drakengard-judgement/1/52.jpg 	new file:   library/drakengard-judgement/1/53.jpg 	new file:   library/drakengard-judgement/1/54.jpg 	new file:   library/drakengard-judgement/1/55.jpg 	new file:   library/drakengard-judgement/1/56.jpg 	new file:   library/drakengard-judgement/1/57.jpg 	new file:   library/drakengard-judgement/1/58.jpg 	new file:   library/drakengard-judgement/1/59.jpg 	new file:   library/drakengard-judgement/1/6.jpg 	new file:   library/drakengard-judgement/1/60.jpg 	new file:   library/drakengard-judgement/1/61.jpg 	new file:   library/drakengard-judgement/1/62.jpg 	new file:   library/drakengard-judgement/1/63.jpg 	new file:   library/drakengard-judgement/1/64.jpg 	new file:   library/drakengard-judgement/1/7.jpg 	new file:   library/drakengard-judgement/1/8.jpg 	new file:   library/drakengard-judgement/1/9.jpg 	new file:   library/drakengard-judgement/1/config.json 	new file:   library/drakengard-judgement/config.json

--- js/constants.js
+++ js/constants.js
@@ -2,8 +2,8 @@
     booksURL can be on a different domain/subdomain if you prefer.
     The books are stored in a library. The library name can be whatever you want (here it's books)
 */
-export function homeURL() {return           "https://drmint.github.io/Okuma-Reader/"}
-export function readerURL() {return         "https://drmint.github.io/Okuma-Reader/read.html"}
-export function booksURL() {return          "https://drmint.github.io/Okuma-Library/books/"}
-export function websiteName() {return       "Okuma Example"}
+export function homeURL() {return           "https://closesttopurple.org/ths"}
+export function readerURL() {return         "https://reader.closesttopurple.org/read.html"}
+export function booksURL() {return          "https://reader.closesttopurple.org/library/"}
+export function websiteName() {return       "THE HUMAN SERVER"}
 export function defaultLanguage() {return   "en"}
