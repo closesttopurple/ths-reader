1b89f59fd21adc6b840d477fa7667189c58c7e6d
closesttopurple <appleapples3500@gmail.com>
2025-05-29 06:59:58 -0400

    Whoopsie, forgot the constants.js to make sure it's for the domain.

--- js/constants.js
+++ js/constants.js
@@ -2,8 +2,8 @@
     booksURL can be on a different domain/subdomain if you prefer.
     The books are stored in a library. The library name can be whatever you want (here it's books)
 */
-export function homeURL() {return           "http://127.0.0.1:5500/"}
-export function readerURL() {return         "http://127.0.0.1:5500/read.html"}
-export function booksURL() {return          "http://127.0.0.1:5500/library/"}
+export function homeURL() {return           "https://reader.closesttopurple.org/"}
+export function readerURL() {return         "https://reader.closesttopurple.org/read.html"}
+export function booksURL() {return          "https://reader.closesttopurple.org/library/"}
 export function websiteName() {return       "THE HUMAN SERVER"}
 export function defaultLanguage() {return   "en"}
