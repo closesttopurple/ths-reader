7d56c6586611fc54e98b88866daf1b2a0ccdcfb1
closesttopurple <appleapples3500@gmail.com>
2025-08-21 04:36:23 -0400

    icon

--- js/constants.js
+++ js/constants.js
@@ -2,8 +2,8 @@
     booksURL can be on a different domain/subdomain if you prefer.
     The books are stored in a library. The library name can be whatever you want (here it's books)
 */
-export function homeURL() {return           "https://reader.closesttopurple.org/"}
-export function readerURL() {return         "https://reader.closesttopurple.org/read.html"}
-export function booksURL() {return          "https://reader.closesttopurple.org/library/"}
+export function homeURL() {return           "http://127.0.0.1:8085/"}
+export function readerURL() {return         "http://127.0.0.1:8085/read.html"}
+export function booksURL() {return          "http://127.0.0.1:8085/library/"}
 export function websiteName() {return       "THE HUMAN SERVER"}
 export function defaultLanguage() {return   "en"}
