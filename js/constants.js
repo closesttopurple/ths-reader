9812b55d5ddb7204a06db52acec88c5acc39dc83
Karis <appleapples3500@gmail.com>
2025-10-04 15:05:17 -0400

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -2,9 +2,10 @@
     booksURL can be on a different domain/subdomain if you prefer.
     The books are stored in a library. The library name can be whatever you want (here it's books)
 */
-export function homeURL() {return           "https://reader.closesttopurple.org/"}
-export function readerURL() {return         "https://reader.closesttopurple.org/read.html"}
-export function booksURL() {return          "https://reader.closesttopurple.org/library/"}
+export function homeURL() {return           "https://reader.thehumanserver.org/"}
+export function readerURL() {return         "https://reader.thehumanserver.org/read.html"}
+export function booksURL() {return          "https://reader.thehumanserver.org/library/"}
 export function websiteName() {return       "THE HUMAN SERVER"}
 
 export function defaultLanguage() {return   "en"}
+
