52dad6e9d4b443cc67f235a68a5d1468fb4dcec5
Karis <appleapples3500@gmail.com>
2026-01-09 22:45:52 -0500

    Update URLs in constants.js for reader subdomain
    uhh broken  ?

--- js/constants.js
+++ js/constants.js
@@ -2,10 +2,11 @@
     booksURL can be on a different domain/subdomain if you prefer.
     The books are stored in a library. The library name can be whatever you want (here it's books)
 */
-export function homeURL() {return           "https://thehumanserver.org/"}
-export function readerURL() {return         "https://thehumanserver.org/read.html"}
-export function booksURL() {return          "https://thehumanserver.org/library/"}
+export function homeURL() {return           "https://reader.thehumanserver.org/"}
+export function readerURL() {return         "https://reader.thehumanserver.org/read.html"}
+export function booksURL() {return          "https://reader.thehumanserver.org/library/"}
 export function websiteName() {return       "THE HUMAN SERVER"}
 
 export function defaultLanguage() {return   "en"}
+
 
