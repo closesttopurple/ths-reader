cd680c1cb85241d32a3165816c94a7c97dfa5f36
closettopurple <appleapples3500@gmail.com>
2026-01-16 00:55:35 -0500

    yay2

--- js/constants.js
+++ js/constants.js
@@ -2,9 +2,9 @@
     booksURL can be on a different domain/subdomain if you prefer.
     The books are stored in a library. The library name can be whatever you want (here it's books)
 */
-export function homeURL() {return           "https://reader.thehumanserver.org/"}
-export function readerURL() {return         "https://reader.thehumanserver.org/read.html"}
-export function booksURL() {return          "https://reader.thehumanserver.org/library/"}
+export function homeURL() {return           "http://192.168.4.27:8032/"}
+export function readerURL() {return         "http://192.168.4.27:8032/read.html"}
+export function booksURL() {return          "http://192.168.4.27:8032/library/"}
 export function websiteName() {return       "THE HUMAN SERVER"}
 
 export function defaultLanguage() {return   "en"}
